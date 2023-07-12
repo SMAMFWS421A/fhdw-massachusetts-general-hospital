d743a3b5e51ff0bc6483f4f34ab5414477a934d4
mffreitag <mikafinn.freitag@mt-itsolutions.com>
2023-07-13 00:38:50 +0200

    Added Custom Api Response to the rest of the Controllers-Classes

--- hospital-be/src/main/java/com/fhdw/hospitalbe/controller/PatientController.java
+++ hospital-be/src/main/java/com/fhdw/hospitalbe/controller/PatientController.java
@@ -2,6 +2,9 @@ package com.fhdw.hospitalbe.controller;
 
 import com.fhdw.hospitalbe.model.Patient;
 import com.fhdw.hospitalbe.service.PatientService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,16 +21,22 @@ public class PatientController {
     }
 
     @GetMapping(path = "{patient_id}")
+    @ApiResponse(responseCode = "200", description = "Found Patient",
+            content = @Content(schema = @Schema(implementation = Patient.class)))
     public ResponseEntity<Patient> getPatient(@PathVariable("patient_id") long patient_id) {
         return new ResponseEntity<Patient>(patientService.getPatient(patient_id), HttpStatus.OK);
     }
 
     @PostMapping
+    @ApiResponse(responseCode = "201", description = "Created Patient",
+            content = @Content(schema = @Schema(implementation = Patient.class)))
     public ResponseEntity<Patient> createPatient(@RequestBody Patient patient) {
         return new ResponseEntity<Patient>(patientService.createPatient(patient), HttpStatus.CREATED);
     }
 
     @DeleteMapping(path = "{patient_id}")
+    @ApiResponse(responseCode = "204", description = "Deleted Patient",
+            content = @Content(schema = @Schema(implementation = Patient.class)))
     public ResponseEntity<Void> deletePatient(@PathVariable("patient_id") long patient_id) {
         patientService.deletePatient(patient_id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- hospital-be/src/main/java/com/fhdw/hospitalbe/controller/VisitController.java
+++ hospital-be/src/main/java/com/fhdw/hospitalbe/controller/VisitController.java
@@ -2,6 +2,9 @@ package com.fhdw.hospitalbe.controller;
 
 import com.fhdw.hospitalbe.model.Visit;
 import com.fhdw.hospitalbe.service.VisitService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,16 +21,22 @@ public class VisitController {
     }
 
     @GetMapping(path = "{visit_id}")
+    @ApiResponse(responseCode = "200", description = "Found Visit",
+            content = @Content(schema = @Schema(implementation = Visit.class)))
     public ResponseEntity<Visit> getVisit(@PathVariable("visit_id") long visit_id) {
         return new ResponseEntity<Visit>(visitService.getVisit(visit_id), HttpStatus.OK);
     }
 
     @PostMapping
+    @ApiResponse(responseCode = "201", description = "Created Visit",
+            content = @Content(schema = @Schema(implementation = Visit.class)))
     public ResponseEntity<Visit> createVisit(@RequestBody Visit visit) {
         return new ResponseEntity<Visit>(visitService.createVisit(visit), HttpStatus.CREATED);
     }
 
     @DeleteMapping(path = "{visit_id}")
+    @ApiResponse(responseCode = "204", description = "Deleted Visit",
+            content = @Content(schema = @Schema(implementation = Visit.class)))
     public ResponseEntity<Void> deleteVisit(@PathVariable("visit_id") long visit_id) {
         visitService.deleteVisit(visit_id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
