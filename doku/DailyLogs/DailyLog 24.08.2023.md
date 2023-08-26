8d9e0c6c5518e2f62655d90d2ca1de2e8373ac0b
LukasHennrichs <lukashennrichs@web.de>
2023-08-26 14:49:02 +0200

    Fertigstellung der Terminplan Seite.  +Hinzufügen von neuen Daily Logs

---
+++ doku/DailyLogs/DailyLog 24.08.2023.md
@@ -0,0 +1,4 @@
+Protokoll zum 24.08.2023
+
+Zuweisung der Aerzteteam und TerminPlan Seite.
+Umwandlung der Struktur des Backends. 
--- hospital-frontend-vue/src/components/TerminPlan.vue
+++ hospital-frontend-vue/src/components/TerminPlan.vue
@@ -1,35 +1,13 @@
 <template>
-  <div>
+  <div class="TerminPlan-container" :style="backgroundImageStyle">
+    <div class="First-Elements">
     <h1>Terminplan</h1>
-    <ul>
-      <li v-for="appointment in appointments" :key="appointment.id">
-        <h3>{{ appointment.date }} - {{ appointment.time }}</h3>
-        <p>Beschreibung: {{ appointment.description }}</p>
-        <p>Arzt: {{ getDoctorName(appointment.doctorId) }}</p>
-        <p>Patient: {{ getPatientName(appointment.patientId) }}</p>
-        <button @click="editAppointment(appointment)">Termin bearbeiten</button>
-        <button @click="deleteAppointment(appointment.id)">Termin löschen</button>
-      </li>
-    </ul>
+
+
+    <h2>Neuen Termin anlegen</h2>
     <button @click="openNewAppointmentForm">Neuen Termin anlegen</button>
-
-
-
-
-    <div v-if="selectedAppointment">
-      <h2>Bearbeite Termin</h2>
-      <p>Datum: <input v-model="selectedAppointment.date"></p>
-      <p>Uhrzeit: <input v-model="selectedAppointment.time"></p>
-      <p>Beschreibung: <input v-model="selectedAppointment.description"></p>
-      <button @click="saveAppointmentChanges">Speichern</button>
-      <button @click="cancelAppointmentEdit">Abbrechen</button>
-    </div>
-
-
-
-
     <div v-if="isNewAppointmentFormOpen">
-      <h2>Neuen Termin anlegen</h2>
+      <h3>Neuer Termin</h3>
       <p>Datum: <input v-model="newAppointment.date"></p>
       <p>Uhrzeit: <input v-model="newAppointment.time"></p>
       <p>Beschreibung: <input v-model="newAppointment.description"></p>
@@ -46,13 +24,89 @@
       <button @click="createNewAppointment">Erstellen</button>
       <button @click="cancelNewAppointment">Abbrechen</button>
     </div>
+
+
+
+    <div v-if="selectedAppointment">
+      <h2>Bearbeite Termin</h2>
+      <p>Datum: <input v-model="selectedAppointment.date"></p>
+      <p>Uhrzeit: <input v-model="selectedAppointment.time"></p>
+      <p>Beschreibung: <input v-model="selectedAppointment.description"></p>
+      <button @click="saveAppointmentChanges">Speichern</button>
+      <button @click="cancelAppointmentEdit">Abbrechen</button>
+    </div>
+    </div>
+
+
+
+    <h2>Bestehende Termine</h2>
+    <ul>
+      <li v-for="appointment in appointments" :key="appointment.id">
+        <h3>{{ appointment.date }} - {{ appointment.time }}</h3>
+        <p>Beschreibung: {{ appointment.description }}</p>
+        <p>Arzt: {{ getDoctorName(appointment.doctorId) }}</p>
+        <p>Patient: {{ getPatientName(appointment.patientId) }}</p>
+        <button @click="editAppointment(appointment)">Bearbeiten</button>
+        <button @click="deleteAppointment(appointment.id)">Löschen</button>
+      </li>
+    </ul>
+
+
+
+
+
+    <h2>Übersicht nach Tag</h2>
+    <ul v-for="(appointments, date) in appointmentsByDay" :key="date">
+      <li>
+        <h3>{{ date }}</h3>
+        <ul>
+          <li v-for="appointment in appointments" :key="appointment.id">
+            <h4>{{ appointment.time }}</h4>
+            <p>Beschreibung: {{ appointment.description }}</p>
+            <p>Arzt: {{ getDoctorName(appointment.doctorId) }}</p>
+            <p>Patient: {{ getPatientName(appointment.patientId) }}</p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+
+
+
+
+    <h2>Übersicht nach Woche</h2>
+    <ul v-for="(appointments, weekStartDate) in appointmentsByWeek" :key="weekStartDate">
+      <li>
+        <h3>Woche ab {{ weekStartDate }}</h3>
+        <ul>
+          <li v-for="appointment in appointments" :key="appointment.id">
+            <h4>{{ appointment.date }} - {{ appointment.time }}</h4>
+            <p>Beschreibung: {{ appointment.description }}</p>
+            <p>Arzt: {{ getDoctorName(appointment.doctorId) }}</p>
+            <p>Patient: {{ getPatientName(appointment.patientId) }}</p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+
+
+
   </div>
 </template>
 
 
-
 <script>
 import dummyDataTerminPlan from '@/assets/dummyDataTerminPlan.json';
+
+
+
+function getWeekStartDate(date) {
+  const d = new Date(date);
+  const day = d.getDay();
+  const diff = d.getDate() - day + (day === 0 ? -6 : 1);
+  return new Date(d.setDate(diff)).toISOString().split('T')[0];
+}
+
+
 
 export default {
   data() {
@@ -69,8 +123,29 @@ export default {
         doctorId: null,
         patientId: null,
       },
+      appointmentsByDay: {},
+      appointmentsByWeek: {},
     };
   },
+
+  computed: {
+    backgroundImagePath() {
+      return require('@/assets/TerminPlanBild.jpg')
+    },
+    backgroundImageStyle(){
+      return{
+        backgroundImage: `url(${this.backgroundImagePath})`,
+        backgroundSize: '100% 100%',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        position: 'relative'
+
+      };
+    },
+  },
+
+
+
   methods: {
     getDoctorName(doctorId) {
       const doctor = this.doctors.find(doctor => doctor.id === doctorId);
@@ -85,47 +160,112 @@ export default {
     },
     saveAppointmentChanges() {
       if (this.selectedAppointment) {
-        const index = this.appointments.findIndex(         appointment => appointment.id === this.selectedAppointment.id       );
+        const index = this.appointments.findIndex(appointment => appointment.id === this.selectedAppointment.id);
         if (index !== -1) {
-                  this.appointments[index] = { ...this.selectedAppointment };
-                  this.selectedAppointment = null;
-            }
-          }
-        },
-        cancelAppointmentEdit() {
+          this.appointments[index] = { ...this.selectedAppointment };
           this.selectedAppointment = null;
-        },
-        deleteAppointment(appointmentId) {
-          const index = this.appointments.findIndex(appointment => appointment.id === appointmentId);
-          if (index !== -1) {
-            this.appointments.splice(index, 1);
-          }
-        },
-        openNewAppointmentForm() {
-          this.isNewAppointmentFormOpen = true;
-        },
-        createNewAppointment() {
-          if (this.newAppointment.date && this.newAppointment.time) {
-            const newId = Math.max(...this.appointments.map(appointment => appointment.id)) + 1;
-            this.newAppointment.id = newId;
-            this.appointments.push({ ...this.newAppointment });
-            this.cancelNewAppointment();
-          }
-        },
-        cancelNewAppointment() {
-          this.newAppointment = {
-            date: '',
-            time: '',
-            description: '',
-            doctorId: null,
-            patientId: null,
-          };
-          this.isNewAppointmentFormOpen = false;
-        },
-      },
+        }
+      }
+    },
+    cancelAppointmentEdit() {
+      this.selectedAppointment = null;
+    },
+    deleteAppointment(appointmentId) {
+      const index = this.appointments.findIndex(appointment => appointment.id === appointmentId);
+      if (index !== -1) {
+        this.appointments.splice(index, 1);
+      }
+    },
+    openNewAppointmentForm() {
+      this.isNewAppointmentFormOpen = true;
+    },
+    createNewAppointment() {
+      if (this.newAppointment.date && this.newAppointment.time) {
+        const newId = Math.max(...this.appointments.map(appointment => appointment.id)) + 1;
+        this.newAppointment.id = newId;
+        this.appointments.push({ ...this.newAppointment });
+        this.cancelNewAppointment();
+      }
+    },
+    cancelNewAppointment() {
+      this.newAppointment = {
+        date: '',
+        time: '',
+        description: '',
+        doctorId: null,
+        patientId: null,
+      };
+      this.isNewAppointmentFormOpen = false;
+    },
+    groupAppointmentsByDay() {
+      this.appointmentsByDay = {};
 
-    };
+
+
+      for (const appointment of this.appointments) {
+        const date = appointment.date;
+        if (!this.appointmentsByDay[date]) {
+          this.appointmentsByDay[date] = [];
+        }
+        this.appointmentsByDay[date].push(appointment);
+      }
+    },
+    groupAppointmentsByWeek() {
+      this.appointmentsByWeek = {};
+
+
+
+      for (const appointment of this.appointments) {
+        const weekStartDate = getWeekStartDate(appointment.date);
+        if (!this.appointmentsByWeek[weekStartDate]) {
+          this.appointmentsByWeek[weekStartDate] = [];
+        }
+        this.appointmentsByWeek[weekStartDate].push(appointment);
+      }
+    },
+  },
+  mounted() {
+    this.groupAppointmentsByDay();
+    this.groupAppointmentsByWeek();
+  },
+};
 </script>
 
+<style>
 
-<style></style>
+.TerminPlan-container{
+  font-family: sans-serif;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0,0,0,0.2);
+  color: darkslategray;
+  z-index: 1;
+}
+
+.TerminPlan-container::before{
+  background-color: rgba(255,255,255,0.2);
+}
+
+
+.First-Elements{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 25vh;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  background-color: #f9f9f9;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+
+</style>
