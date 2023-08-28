24f964d62e6483eced7b7380f5fe89ca90e6bd5e
Marco <129759421+marco15t@users.noreply.github.com>
2023-08-28 19:09:54 +0200

    fixed test3

--- hospital-be/src/test/java/com/fhdw/hospitalbe/integration/PatientControllerTest.java
+++ hospital-be/src/test/java/com/fhdw/hospitalbe/integration/PatientControllerTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fhdw.hospitalbe.DatabaseTestUtil;
 import com.fhdw.hospitalbe.model.Patient;
 import com.fhdw.hospitalbe.model.builder.PatientBuilder;
+import com.fhdw.hospitalbe.model.builder.PatientRecordBuilder;
 import com.fhdw.hospitalbe.model.mapper.PatientMapper;
 import com.fhdw.hospitalbe.repository.PatientRepository;
 import com.fhdw.hospitalbe.repository.table.AppointmentTable;
@@ -102,6 +103,28 @@ public class PatientControllerTest {
     }
 
     @Test
+    public void createPatientWithRecordTest() throws Exception {
+        Patient patient = new PatientBuilder().firstName("First").lastName("Last").patientRecord(new PatientRecordBuilder().medication("Test").build()).build();
+
+        mockMvc.perform(post("/api/v1/patient")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(patient))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(result -> {
+                    Patient patientResponse = objectMapper.readValue(result.getResponse().getContentAsString(), Patient.class);
+                    Assertions.assertEquals(patient.getLastName(), patientResponse.getLastName());
+                    Assertions.assertEquals(patient.getPatientRecord().getMedication(), patientResponse.getPatientRecord().getMedication());
+                    Assertions.assertNotNull(patientResponse.getId());
+                });
+
+        PatientTable patientDb = patientRepository.findOne(Example.of(PatientMapper.toTable(patient))).get();
+        Assertions.assertEquals(patient.getLastName(), patientDb.getLastName());
+        Assertions.assertEquals(patient.getPatientRecord().getMedication(), patientDb.getPatientRecord().getMedication());
+        Assertions.assertNotNull(patientDb.getId());
+    }
+
+    @Test
     public void updatePatientTest() throws Exception {
         Patient patient = PatientMapper.fromTable(databaseTestUtil.createPatientWithRecord());
 
@@ -142,7 +165,7 @@ public class PatientControllerTest {
         AppointmentTable a = databaseTestUtil.createAppointment(d, p.getPatientRecord());
         VisitTable v = databaseTestUtil.createVisit(d, p.getPatientRecord());
 
-        mockMvc.perform(delete("/api/v1/patient/" + d.getId())
+        mockMvc.perform(delete("/api/v1/patient/" + p.getId())
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent())
